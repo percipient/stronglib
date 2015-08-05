637e4ccb05c59f1fcfcd1c9221fab24de594485f
Qiuwen "Owen" Fan <fanqiuwen@gmail.com>
2015-08-05 11:02:07 -0400

    Add tests to improve coverage

--- tests/test_common.py
+++ tests/test_common.py
@@ -8,7 +8,7 @@ import responses
 from six.moves.urllib.parse import parse_qs, urlparse
 
 import strongarm
-from strongarm.common import PaginatedResourceList
+from strongarm.common import Struct, PaginatedResourceList
 
 
 class StrongarmTestCase(unittest.TestCase):
@@ -23,6 +23,27 @@ class StrongarmTestCase(unittest.TestCase):
         with self.assertRaises(strongarm.StrongarmUnauthorized):
             strongarm.api_key = 'bad_token'
             strongarm.Domain.all()
+
+
+class StructTestCase(unittest.TestCase):
+
+    def test_recursive_traversal(self):
+        """
+        Test that the struct faithfully represent the structure of the given
+        dictionary.
+
+        """
+        d = {'a': 1,
+             'b': {'c': 2, 'd': 3},
+             'e': {'f': {'g': ('test', 42)}},
+             'h': 'value'}
+
+        struct = Struct(d)
+        self.assertEqual(struct.a, 1)
+        self.assertEqual(struct.b.c, 2)
+        self.assertEqual(struct.b.d, 3)
+        self.assertEqual(struct.e.f.g, ('test', 42))
+        self.assertEqual(struct.h, 'value')
 
 
 class PaginationTestCase(unittest.TestCase):
@@ -61,6 +82,15 @@ class PaginationTestCase(unittest.TestCase):
                                callback=paginated_resource,
                                content_type='application/json')
 
+    def lazy_pages(self, index):
+        """
+        How many pages the lazy list should have requested to get to index.
+
+        """
+        if index < 0:
+            index += self.total
+        return index // self.per_page + 1
+
     @responses.activate
     def test_init_lazy(self):
         """
@@ -81,19 +111,33 @@ class PaginationTestCase(unittest.TestCase):
         self.plist = PaginatedResourceList(int, self.endpoint)
 
         self.assertEqual(self.plist[2], 2)
-        self.assertEqual(len(responses.calls), 1)
+        self.assertEqual(len(responses.calls), self.lazy_pages(2))
 
         self.assertEqual(self.plist[5], 5)
-        self.assertEqual(len(responses.calls), 2)
+        self.assertEqual(len(responses.calls), self.lazy_pages(5))
 
         self.assertEqual(self.plist[10], 10)
-        self.assertEqual(len(responses.calls), 3)
+        self.assertEqual(len(responses.calls), self.lazy_pages(10))
 
         self.assertEqual(self.plist[13], 13)
-        self.assertEqual(len(responses.calls), 4)
+        self.assertEqual(len(responses.calls), self.lazy_pages(13))
 
     @responses.activate
-    def test_index_error(self):
+    def test_index_negative(self):
+        """
+        Test that when negative indices are handled correctly and lazily.
+
+        """
+        self.plist = PaginatedResourceList(int, self.endpoint)
+
+        self.assertEqual(self.plist[-12], self.total - 12)
+        self.assertEqual(len(responses.calls), self.lazy_pages(-12))
+
+        self.assertEqual(self.plist[-1], self.total - 1)
+        self.assertEqual(len(responses.calls), self.lazy_pages(-1))
+
+    @responses.activate
+    def test_index_out_of_bounds(self):
         """
         Test that when an out-of-bounds index is passed in, an error is thrown
         without requesting additional pages.
@@ -105,6 +149,19 @@ class PaginationTestCase(unittest.TestCase):
         self.assertEqual(len(responses.calls), 1)
 
     @responses.activate
+    def test_index_type_error(self):
+        """
+        Test that when an incorrectly typed index is passed in, an error is
+        thrown without requesting additional pages.
+
+        """
+        self.plist = PaginatedResourceList(int, self.endpoint)
+
+        self.assertRaises(TypeError, lambda: self.plist['a'])
+        self.assertEqual(len(responses.calls), 1)
+
+
+    @responses.activate
     def test_slice_lazy(self):
         """
         Test that when it's being sliced, the paginated list only requests the
@@ -114,13 +171,13 @@ class PaginationTestCase(unittest.TestCase):
         self.plist = PaginatedResourceList(int, self.endpoint)
 
         self.assertEqual(self.plist[1:3], list(range(1, 3)))
-        self.assertEqual(len(responses.calls), 1)
+        self.assertEqual(len(responses.calls), self.lazy_pages(2))
 
         self.assertEqual(self.plist[1:7:2], list(range(1, 7, 2)))
-        self.assertEqual(len(responses.calls), 2)
+        self.assertEqual(len(responses.calls), self.lazy_pages(6))
 
         self.assertEqual(self.plist[10:13], list(range(10, 13)))
-        self.assertEqual(len(responses.calls), 4)
+        self.assertEqual(len(responses.calls), self.lazy_pages(12))
 
     @responses.activate
     def test_list_cast(self):
@@ -133,4 +190,4 @@ class PaginationTestCase(unittest.TestCase):
 
         entire_list = list(self.plist)
         self.assertEqual(entire_list, list(range(self.total)))
-        self.assertEqual(len(responses.calls), 4)
+        self.assertEqual(len(responses.calls), self.lazy_pages(self.total-1))
