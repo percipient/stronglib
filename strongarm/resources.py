03011c04af3a0929b9ab049488dedf121bda7b64
Patrick Cloke <clokep@patrick.cloke.us>
2018-08-16 09:02:54 -0400

    Start adding v2 API information.

--- strongarm/resources.py
+++ strongarm/resources.py
@@ -8,6 +8,18 @@ class Domain(StrongResource, CreatableResource, DeletableResource, ListableResou
     # The domain name is used as the unique identifier passed in the url.
     id_attr = 'name'
 
+    # Constants for use with the status field.
+    # A blacklisted domain will be redirected to the Strongarm blackhole when
+    # queried. Strongarm users will be sent an alert when a user attempts to
+    # visit a blacklisted domain.
+    BLACKLISTED = 'blacklisted'
+    # A whitelisted domain will always recursively resolve, even if the domain
+    # is blocked based on one of the Strongarm feeds or due to content filtering
+    # policy.
+    WHITELISTED = 'whitelisted'
+    # A filtered domain will be blocked for content filtering.
+    FILTERED = 'filtered'
+
 
 class Infection(StrongResource, ListableResource):
     endpoint = '/api/infections/'
